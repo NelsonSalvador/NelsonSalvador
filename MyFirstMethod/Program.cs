4cfefda412abe1bf0a9ffe7dd9d895d1d889d856
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-04-02 10:46:55 +0100

    Documentation

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -4,11 +4,18 @@ namespace MyFirstMethod
 {
     class Program
     {
+        /// <summary>
+        /// This is the main Method, the program starts here.
+        /// </summary>
+        /// <param name="args">Not used in this program.</param>
         static void Main(string[] args)
         {
             CountTo10();
             CountTo10();
         }
+        /// <summary>
+        /// Outputs numbers from 1 to 10
+        /// </summary>
         static void CountTo10 ()
         {
             for (int i = 1 ; i <= 10; i ++)
