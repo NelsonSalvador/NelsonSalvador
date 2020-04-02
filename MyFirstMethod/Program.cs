9c4a6608495cf3289e42b8a9609bdc5c33cb5d53
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-04-02 11:08:44 +0100

    Add overload method

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -11,7 +11,7 @@ namespace MyFirstMethod
         static void Main(string[] args)
         {
             CountTo10();
-            CountTo10();
+            CountToN(3, 7);
         }
         /// <summary>
         /// Outputs numbers from 1 to 10
@@ -22,7 +22,20 @@ namespace MyFirstMethod
             {
                 Console.WriteLine(i);
             }
-        }   
+        }
+
+        /// <summary>
+        /// Outputs numbers from n to n depending on the input
+        /// </summary>
+        /// <param name="n1">Min number</param>
+        /// <param name="n2">Max number</param>
+        static void CountToN(int n1, int n2)
+        {
+            for (int i = n1; i <= n2; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
 
     }
 }
